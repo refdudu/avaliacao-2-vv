2763f554192deeddd07a76c7162fadf2a7dde5c4
Renan Fischer <renan.fischer.2013@gmail.com>
2025-10-13 15:22:12 -0300

    RED criação teste "cria um produto no inventário"

---
+++ src/Inventory.test.ts
@@ -0,0 +1,22 @@
+describe("Gerenciamento de inventário", () => {
+  let inventoryManager: InventoryManager;
+  beforeEach(() => {
+    inventoryManager = new InventoryManager();
+  });
+
+  it("cria um produto no inventário", () => {
+    const quantity = 10;
+    const price = 100;
+    const product: ProductDTO = {
+      name: "Produto A",
+      price,
+      quantity
+    };
+    inventoryManager.createProduct(product);
+    const items = inventoryManager.getProducts();
+    expect(items).toHaveLength(1);
+    expect(items[0]).toHaveProperty("id");
+    expect(items[0].quantity).toBe(quantity);
+    expect(items[0].price).toBe(price);
+  });
+});
