94f1b3f40d0f73ce70cf5e329eabb764b18c3eb2
Renan Fischer <renan.fischer.2013@gmail.com>
2025-10-15 16:12:32 -0300

    RED teste para deletar produto do inventário

--- src/Inventory.test.ts
+++ src/Inventory.test.ts
@@ -1,3 +1,4 @@
+import { ProductNotFoundError } from "./errors/ProductNotFoundError";
 import { InventoryManager } from "./InventoryManager";
 import { ProductDTO } from "./Product";
 
@@ -13,7 +14,7 @@ describe("Gerenciamento de inventário", () => {
     const product: ProductDTO = {
       name: "Produto A",
       price,
-      quantity
+      quantity,
     };
     inventoryManager.createProduct(product);
     const items = inventoryManager.getProducts();
@@ -24,11 +25,31 @@ describe("Gerenciamento de inventário", () => {
   });
 
   it("adiciona quantidade a um produto existente", () => {
-  const productDTO: ProductDTO = { name: "Produto A", price: 100, quantity: 10 };
-  const createdProduct = inventoryManager.createProduct(productDTO);
-  const quantityToAdd = 5;
-  inventoryManager.addProductQuantity(createdProduct.id, quantityToAdd);
-  const product = inventoryManager.findProductById(createdProduct.id);
-  expect(product?.quantity).toBe(productDTO.quantity + quantityToAdd);
-});
+    const productDTO: ProductDTO = {
+      name: "Produto A",
+      price: 100,
+      quantity: 10,
+    };
+    const createdProduct = inventoryManager.createProduct(productDTO);
+    const quantityToAdd = 5;
+    inventoryManager.addProductQuantity(createdProduct.id, quantityToAdd);
+    const product = inventoryManager.findProductById(createdProduct.id);
+    expect(product?.quantity).toBe(productDTO.quantity + quantityToAdd);
+  });
+  it("deleta produto do inventário", () => {
+    const productDTO: ProductDTO = {
+      name: "Produto A",
+      price: 100,
+      quantity: 10,
+    };
+    const createdProduct = inventoryManager.createProduct(productDTO);
+    const searchedProduct = inventoryManager.findProductById(createdProduct.id);
+
+    expect(searchedProduct).toBeDefined();
+    inventoryManager.deleteProduct(searchedProduct.id);
+
+    expect(() => inventoryManager.findProductById(searchedProduct.id)).toThrow(
+      ProductNotFoundError
+    );
+  });
 });
