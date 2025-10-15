dc79757bd24be79e64fcde3091811c05985454fd
Renan Fischer <renan.fischer.2013@gmail.com>
2025-10-14 21:35:54 -0300

    GREEN criações de métodos para adicionar quantidade e buscar produto por ID

--- src/InventoryManager.ts
+++ src/InventoryManager.ts
@@ -2,7 +2,15 @@ import { Product, ProductDTO } from "./Product";
 
 export class InventoryManager {
   private products: Product[] = [];
-
+  findProductById(id: string): Product {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) throw new Error("Product not found");
+    return product;
+  }
+  addProductQuantity(id: string, quantityToAdd: number): void {
+    const product = this.findProductById(id);
+    product.quantity += quantityToAdd;
+  }
   createProduct(productDTO: ProductDTO): Product {
     const newProduct = new Product(productDTO);
     this.products.push(newProduct);
