4f94b64c906fd47896c877b2a4c1b590b0f96a66
Renan Fischer <renanfisher@atendare.com>
2025-10-17 16:33:47 -0300

    RED criação do teste cria um usuário

---
+++ src/UserManager.test.ts
@@ -0,0 +1,20 @@
+describe("Gerenciamento de usuários", () => {
+  let userManager: UserManager;
+  beforeEach(() => {
+    userManager = new UserManager();
+  });
+
+  it("cria um usuário", () => {
+    const user: UserDTO = {
+      name: "Usuário A",
+    };
+    const newUser = userManager.createUser(user);
+
+    const items = userManager.getUsers();
+    expect(items).toHaveLength(1);
+    expect(items[0]).toHaveProperty("id");
+
+    expect(items[0].name).toBe(newUser.name);
+    expect(items[0].id).toBe(newUser.id);
+  });
+});
